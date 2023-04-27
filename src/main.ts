4baf91f9f5f70e6af38d0c439ceffc247b6915b7
Evgeny Kolotov <94759875+evgenykolotov@users.noreply.github.com>
2023-04-27 20:28:38 +0300

    feat: Добавлен линтинг и тестирование кода с помощью «Github Actions» (#1)
    * feat: Добавлен линтинг и тестирование кода с помощью «Github Actions»
    
    * feat: Добавлен деплой исходного кода на сервер с помощью «Github Actions»
    
    * feat: Настроен выпуск релизов с помощью «Semantic-release» «Github Actions»
    
    * feat: Добавлен npm-пакет «@nestjs/swagger»
    
    * feat: Dockerfiles с описанием рабочих контейнеров

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
+	/** Создание NestJS приложения */
 	const app = await NestFactory.create(AppModule);
+
+	/** Конфигурирование пакета Swagger */
+	const config = new DocumentBuilder()
+		.setTitle('Telegram API Gateway')
+		.setDescription('OpenAPI for Telegram Application')
+		.setVersion('1.0.0')
+		.build();
+
+	/** Создание документа SwaggerModule */
+	const document = SwaggerModule.createDocument(app, config);
+
+	/** Генерация страницы с OpenAPI */
+	SwaggerModule.setup('/api/swagger', app, document);
+
+	/** Прослушивание порта 3000 */
 	await app.listen(3000);
 }
+
 bootstrap();
