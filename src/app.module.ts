0f6595af269c03fba3c500b20d81123e886c5e22
Evgeny Kolotov <evgenykolotov.it@gmail.com>
2023-05-11 21:03:19 +0300

    feat: Добавлена конфигурация для подключения к RabbitMQ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
+import { RMQModule } from 'nestjs-rmq';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { getRMQConfig } from '@config/rmq.config';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: `environment/.${process.env.NODE_ENV}.env`,
+		}),
+		RMQModule.forRootAsync(getRMQConfig()),
+	],
 })
 export class AppModule {}
